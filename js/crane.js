7cca50a91fe11aa28a8f0f33d50a0d147bf73ec7
高洁 <tj.gaojie@outlook.com>
2017-08-14 17:40:37 +0800

    222

--- js/crane.js
+++ js/crane.js
@@ -1,5 +1,5 @@
 var siteUrl = "https://wudou.must-try.me/";
-var imageUrl = 'http://wudou.must-try.me/';
+var imageUrl = 'https://wudou.must-try.me/';
 var globalSort = "views";
 var globalChannelId = undefined;
 var globalPage = 1;
